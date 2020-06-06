4298f73e5b93d523ea0e78c61ff68a22323c8887
danfrenette <dan.r.frenette@gmail.com>
2020-06-06 17:52:36 -0400

    Trailing commas are gone but I'll deal for now

--- App.js
+++ App.js
@@ -1,42 +1,35 @@
 const Pet = props => {
-  const { animal, breed, name } = props
+  const { animal, breed, name } = props;
   return React.createElement(
     "div",
     {},
     React.createElement("h1", {}, [
       React.createElement("h1", {}, name),
       React.createElement("h2", {}, animal),
-      React.createElement("h2", {}, breed),
+      React.createElement("h2", {}, breed)
     ])
-  )
-}
+  );
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Adopt Me You Fuck!"),
-      React.createElement( Pet, {
-        name: "suh dude",
-        animal: "Dog",
-        breed: "unicorn"
-      }),
-      React.createElement( Pet, {
-        name: "bruhh",
-        animal: "explosion",
-        breed: "fiery"
-      }),
-      React.createElement( Pet, {
-        name: "Doink",
-        animal: "cat",
-        breed: "unicorn"
-      }),
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me You Fuck!"),
+    React.createElement(Pet, {
+      name: "suh dude",
+      animal: "Dog",
+      breed: "unicorn"
+    }),
+    React.createElement(Pet, {
+      name: "bruhh",
+      animal: "explosion",
+      breed: "fiery"
+    }),
+    React.createElement(Pet, {
+      name: "Doink",
+      animal: "cat",
+      breed: "unicorn"
+    })
+  ]);
+};
 
-ReactDOM.render(
-  React.createElement(App),
-  document.getElementById("root"),
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
