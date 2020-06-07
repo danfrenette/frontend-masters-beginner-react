424ff734e38e0f03e43d79d7aee842467abbabf2
danfrenette <dan.r.frenette@gmail.com>
2020-06-06 20:57:27 -0400

    Add search params, it uses a hook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>This tag will be overwritten by CSS</h1>
-      <Pet name="supa" animal="hot" breed="Fire" />
-      <Pet name="supa" animal="hot" breed="Fire" />
-      <Pet name="supa" animal="hot" breed="Fire" />
+      <SearchParams />
     </div>
   );
 };
 
-render(<App/>, document.getElementById("root"));
+render(<App />, document.getElementById("root"));
