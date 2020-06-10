2cc0a608eb247dbdef18450a6299f903fa9c9912
danfrenette <dan.r.frenette@gmail.com>
2020-06-10 18:41:59 -0400

    Style details page

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,53 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = props => {
-  return <h1>Bien venido power bottoms!</h1>
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { loading: true };
+  }
+
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      const {
+        name,
+        type,
+        description,
+        photos,
+        breeds,
+        contact: { address }
+      } = animal;
+
+      this.setState({
+        name: name,
+        animal: type,
+        location: `${address.city}, ${address.city}`,
+        description: description,
+        media: photos,
+        breed: breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h1>Loading the floof</h1>;
+    }
+    const { animal, breed, location, description, name } = this.state
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Details;
