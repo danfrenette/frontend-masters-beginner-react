b725d5b88f92e2e61db997e0969216d2c6b4dc35
danfrenette <dan.r.frenette@gmail.com>
2020-06-10 18:04:42 -0400

    Style Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-export default function Pet({ animal, breed, name }) {
+export default function Pet({ animal, breed, name, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
+
+  if (media.length) {
+    hero = media[0].small;
+  }
+
   return (
-    <div>
-      <h1>{name}</h1>
-      <h2>{animal}</h2>
-      <h2>{breed}</h2>
-    </div>
+    <a className="pet" href={`/details/${id}`}>
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
-};
+}
